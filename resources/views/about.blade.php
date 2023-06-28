a9b0ef8ee2c4a1f448b6f51102b9124c89fe175f
maddiganesh <maddiganesh009@gmail.com>
2023-06-28 22:20:36 +0530

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About page</title>
+    <link href="styles/style.css" rel="stylesheet">
+</head>
+<body>
+    <section class="contaner">
+    <h1>This is about page..!!! </h1>
+    </section>
+    <section  class="menu">
+    <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">contact</a></li>
+            <li><a href="/history">History</a></li>
+    </ul>
+
+    </section>
+</body>
+</html>
